926c7da443339083bfa79c87e827b8bd76264330
Resecitud1997 <resecitud@gmail.com>
2025-12-07 19:54:59 -0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '50px' }}>
+      <h1>¡Hola! Tu Frontend ya funciona.</h1>
+      <p>Compilado exitosamente en Cloudflare Pages.</p>
+    </div>
+  </React.StrictMode>
+);
